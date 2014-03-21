203216d48553f92bdda2ff92f5e3b3c934d15f9f
Jason DeLaat <jason.develops@gmail.com>
2014-03-21 08:40:36 -0400

    Changed email address in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='PyMonad',
     version='1.0dev',
     author='Jason DeLaat',
-    author_email='jason.delaat@gmail.com',
+    author_email='jason.develops@gmail.com',
     packages=['pymonad', 'pymonad.test'],
     url='http://pypi.python.org/pypi/PyMonad',
     license=open('LICENSE.txt').read(),
