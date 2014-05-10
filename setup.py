d9eda21228a7564ebd604abc148ceb88e35dbd0d
Jason DeLaat <jason.delaat@gmail.com>
2014-05-10 07:39:45 -0400

    Updated setup.py to version 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyMonad',
-    version='1.1',
+    version='1.2',
     author='Jason DeLaat',
     author_email='jason.develops@gmail.com',
     packages=['pymonad', 'pymonad.test'],
