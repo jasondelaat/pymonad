0e0c84db9b61c76c89938756d5b4b06d8d79dc3e
Jason DeLaat <jason.delaat@gmail.com>
2014-05-11 10:33:45 -0400

    Implemented Writer monad and tests including MonadTester to help make monad tests more readable.

---
+++ pymonad/test/test_Writer.py
@@ -0,0 +1,51 @@
+# --------------------------------------------------------
+# (c) Copyright 2014 by Jason DeLaat. 
+# Licensed under BSD 3-clause licence.
+# --------------------------------------------------------
+
+import unittest
+from pymonad.Writer import *
+from MonadTester import *
+
+class TestWriterFunctor(unittest.TestCase, MonadTester):
+	def __init__(self, x):
+		super(TestWriterFunctor, self).__init__(x)
+		self.setClassUnderTest(NumberWriter)
+
+	def testFunctorLaws(self):
+		self.given((8, 5))
+		self.ensure_first_functor_law_holds()
+		self.ensure_second_functor_law_holds()
+
+class TestWriterApplicative(unittest.TestCase, MonadTester):
+	def __init__(self, x):
+		super(TestWriterApplicative, self).__init__(x)
+		self.setClassUnderTest(NumberWriter)
+
+	def testApplicativeLaws(self):
+		self.given((8, 5))
+		self.ensure_first_applicative_law_holds()
+		self.ensure_second_applicative_law_holds()
+		self.ensure_third_applicative_law_holds()
+		self.ensure_fourth_applicative_law_holds()
+		self.ensure_fifth_applicative_law_holds()
+
+class TestWriterMonad(unittest.TestCase, MonadTester):
+	def __init__(self, x):
+		super(TestWriterMonad, self).__init__(x)
+		self.setClassUnderTest(StringWriter)
+
+	def monad_function_f(self, x):
+		return Writer((x / 10, "Division successful."))
+
+	def monad_function_g(self, x):
+		return Writer((x * 10, "Multiplication successful."))
+
+	def testMonadLaws(self):
+		self.given((8, "dummy"))
+		self.ensure_first_monad_law_holds()
+		self.ensure_second_monad_law_holds()
+		self.ensure_third_monad_law_holds()
+
+if __name__ == "__main__":
+	unittest.main()
