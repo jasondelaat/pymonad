285d46fbaf77ca8017800d1cdb566ef40d576427
Jason DeLaat <jason.develops@gmail.com>
2014-06-21 07:48:56 -0400

    Added tests for the Maybe, First and Last Monoids.

--- pymonad/test/test_Maybe.py
+++ pymonad/test/test_Maybe.py
@@ -90,6 +90,91 @@ class TestMaybeUnit(unittest.TestCase):
 	def testUnitOnNothing(self):
 		self.assertEqual(Nothing.unit(8), Just(8))
 		self.assertEqual(unit(Nothing, 8), Just(8))
+class TestMaybeMonoid(unittest.TestCase, MonoidTester):
+	def test_mzero(self):
+		self.givenMonoid(Maybe)
+		self.get_mzero()
+		self.ensure_mzero_is(Nothing)
+
+	def test_right_identity(self):
+		self.givenMonoid(Just(9))
+		self.ensure_monoid_plus_zero_equals(Just(9))
+
+	def test_left_identity(self):
+		self.givenMonoid(Just(9))
+		self.ensure_zero_plus_monoid_equals(Just(9))
+
+	def test_associativity(self):
+		self.givenMonoids(Just(1), Just(2), Just(3))
+		self.ensure_associativity()
+	
+	def test_mplus_with_two_just_values(self):
+		self.givenMonoids(Just(1), Just(2))
+		self.ensure_mconcat_equals(Just(3))
+
+	def test_mplus_with_one_just_and_one_nothing(self):
+		self.givenMonoids(Just(1), Nothing)
+		self.ensure_mconcat_equals(Just(1))
+
+class TestFirstMonoid(unittest.TestCase, MonoidTester):
+	def test_mzero(self):
+		self.givenMonoid(First)
+		self.get_mzero()
+		self.ensure_mzero_is(First(Nothing))
+
+	def test_right_identity(self):
+		self.givenMonoid(First(Just(9)))
+		self.ensure_monoid_plus_zero_equals(First(Just(9)))
+
+	def test_left_identity(self):
+		self.givenMonoid(First(Just(9)))
+		self.ensure_zero_plus_monoid_equals(First(Just(9)))
+
+	def test_associativity(self):
+		self.givenMonoids(First(Just(1)), First(Just(2)), First(Just(3)))
+		self.ensure_associativity()
+	
+	def test_mplus_with_two_just_values(self):
+		self.givenMonoids(First(Just(1)), First(Just(2)))
+		self.ensure_mconcat_equals(First(Just(1)))
+
+	def test_mplus_with_just_and_nothing(self):
+		self.givenMonoids(First(Just(1)), Nothing)
+		self.ensure_mconcat_equals(First(Just(1)))
+
+	def test_mplus_with_nothing_and_just(self):
+		self.givenMonoids(Nothing, First(Just(1)))
+		self.ensure_mconcat_equals(First(Just(1)))
+
+class TestLastMonoid(unittest.TestCase, MonoidTester):
+	def test_mzero(self):
+		self.givenMonoid(Last)
+		self.get_mzero()
+		self.ensure_mzero_is(Last(Nothing))
+
+	def test_right_identity(self):
+		self.givenMonoid(Last(Just(9)))
+		self.ensure_monoid_plus_zero_equals(Last(Just(9)))
+
+	def test_left_identity(self):
+		self.givenMonoid(Last(Just(9)))
+		self.ensure_zero_plus_monoid_equals(Last(Just(9)))
+
+	def test_associativity(self):
+		self.givenMonoids(Last(Just(1)), Last(Just(2)), Last(Just(3)))
+		self.ensure_associativity()
+	
+	def test_mplus_with_two_just_values(self):
+		self.givenMonoids(Last(Just(1)), Last(Just(2)))
+		self.ensure_mconcat_equals(Last(Just(2)))
+
+	def test_mplus_with_just_and_nothing(self):
+		self.givenMonoids(Last(Just(1)), Nothing)
+		self.ensure_mconcat_equals(Last(Just(1)))
+
+	def test_mplus_with_nothing_and_just(self):
+		self.givenMonoids(Nothing, Last(Just(1)))
+		self.ensure_mconcat_equals(Last(Just(1)))
 
 if __name__ == "__main__":
 	unittest.main()
