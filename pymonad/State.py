bc627e657296d952e3462499252096426a70c0c1
Jason DeLaat <jason.develops@gmail.com>
2014-06-23 12:03:12 -0400

    Added docstrings to State.py

--- pymonad/State.py
+++ pymonad/State.py
@@ -6,7 +6,14 @@
 from pymonad.Monad import *
 
 class State(Monad):
+	""" Represents a calculation which produces a stateful side-effect.  """
+
 	def fmap(self, function): 
+		"""
+		Applies 'function' to the result contained within the monad and passes the state
+		along unchanged.
+
+		"""
 		@State
 		def newState(state):
 			result, st = self(state)
@@ -14,6 +21,11 @@ class State(Monad):
 		return newState
 
 	def amap(self, functorValue):
+		"""
+		Applies the function contained within the monad to the result of 'functorValue'
+		and passes along the state unchanged.
+
+		"""
 		@State
 		def newState(state):
 			function = self.getResult(state)
@@ -22,6 +34,12 @@ class State(Monad):
 		return newState
 
 	def bind(self, function): 
+		"""
+		Chains together a series of stateful computations. 'function' accepts a single value
+		and produces a new 'State' value which may or may not alter the state when it is
+		executed.
+
+		"""
 		@State
 		def newState(state):
 			result, st = self(state)
@@ -30,16 +48,34 @@ class State(Monad):
 
 	@classmethod
 	def unit(cls, value): 
+		"""
+		Produces a new stateful calculation which produces 'value' and leaves the passed in 
+		state untouched.
+
+		"""
 		return State(lambda state: (value, state))
 
 	def getResult(self, state):
+		""" Returns only the result of a stateful calculation, discarding the state. """
 		return self.value(state)[0]
 
 	def getState(self, state):
+		""" Returns only the final state of a stateful calculation, discarding the result.  """
 		return self.value(state)[1]
 
 	def __call__(self, state): 
+		"""
+		Executes the stateful calculation contained within the monad with an initial 'state'.
+		Returns the result and the final state as a 2-tuple.
+
+		"""
 		return self.value(state)
 
 	def __eq__(self, other):
+		"""
+		Always raises a TypeError.
+		The State monad contains functions which can not be directly compared for equality,
+		so attempting to compare instances of State with anything will always fail.
+
+		"""
 		raise TypeError("State: Can't compare functions for equality.")
