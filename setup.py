9fdc9609ce9660e5e753dd402c24de531283d15e
Jason DeLaat <jason.develops@gmail.com>
2014-06-28 07:35:27 -0400

    Changed version to 1.3 and changed url to bitbucket.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='PyMonad',
-    version='1.3dev',
+    version='1.3',
     author='Jason DeLaat',
     author_email='jason.develops@gmail.com',
     packages=['pymonad', 'pymonad.test'],
-    url='http://pypi.python.org/pypi/PyMonad',
+    url='https://bitbucket.org/jason_delaat/pymonad',
     license=open('LICENSE.txt').read(),
     description='Collection of classes for programming with functors, applicative functors and monads.',
     long_description=open('README.txt').read() + open("CHANGES.txt").read(),
