d652e01deae68f72a771841bb942df26458c11d0
Jason DeLaat <jason.delaat@gmail.com>
2020-05-16 13:53:39 -0400

    Removed Container, Functor, and Applicative classes and made Monad and Monoid each self contained.

--- pymonad/Monad.py
+++ pymonad/Monad.py
@@ -3,64 +3,105 @@
 # Licensed under BSD 3-clause licence.
 # --------------------------------------------------------
 
-from pymonad.Applicative import *
+class Monad:
+    """
+    Represents a "context" in which calculations can be executed.
 
-class Monad(Applicative):
-	"""
-	Represents a "context" in which calculations can be executed.
+    You won't create 'Monad' instances directly. Instead, sub-classes implement
+    specific contexts. Monads allow you to bind together a series of calculations
+    while maintaining the context of that specific monad.
 
-	You won't create 'Monad' instances directly. Instead, sub-classes implement
-	specific contexts. Monads allow you to bind together a series of calculations
-	while maintaining the context of that specific monad.
+    """
+    def __init__(self, value):
+        """ Wraps 'value' in the Monad's context. """
+        self.value = value
+        self.is_monad_value = True
 
-	"""
+    def fmap(self, function): 
+        """ Applies 'function' to the contents of the functor and returns a new functor value. """
+        raise NotImplementedError("'fmap' not defined.")
+    
+    def __rmul__(self, aFunction):
+        """ 
+        The 'fmap' operator.
+        The following are equivalent:
 
-	def __init__(self, value):
-		""" Wraps 'value' in the Monad's context. """
-		super(Monad, self).__init__(value)
-		self.is_monad_value = True
+            aFunctor.fmap(aFunction)
+            aFunction * aFunctor
 
-	def bind(self, function):
-		""" Applies 'function' to the result of a previous monadic calculation. """
-		raise NotImplementedError
+        """
+        
+        return self.fmap(aFunction)
 
-	def then(self, function):
-		""" Combines the functionality of bind and fmap.
+    @classmethod
+    def unit(cls, value):
+        """ Returns an instance of the Functor with 'value' in a minimum context.  """
+        raise NotImplementedError
 
-		Instead of worrying about whether to use bind or fmap,
-		users can just use the then method to chain function
-		calls together. The then method uses attempts to use
-		bind first and if that doesn't work, uses fmap
-		instead.
+    def amap(self, functorValue):
+        """ 
+        Applies the function stored in the functor to the value inside 'functorValue'
+        returning a new functor value.
 
-		Args:
-		function: A python function or lambda expression
-		which returns either a build-in type (int, string,
-		etc.) or an appropriate monad type (Maybe, Either,
-		etc.)
+        """
+        raise NotImplementedError
 
-		Returns:
-		A monad value of the same type as 'self'
-		"""
+    def __and__(self, functorValue):
+        """ The 'amap' operator. """
+        return self.amap(functorValue)
+    
+    def getValue(self):
+        """ Returns the value held by the Container. """
+        return self.value
 
-		result = self.bind(function)
-		try:
-			result.is_monad_value
-			return result
-		except AttributeError:
-			return self.fmap(function)
+    def __eq__(self, other):
+        return self.value == other.value
 
-	def __rshift__(self, function):
-		""" 
-		The 'bind' operator. The following are equivalent:
-			monadValue >> someFunction
-			monadValue.bind(someFunction)
+    def bind(self, function):
+        """ Applies 'function' to the result of a previous monadic calculation. """
+        raise NotImplementedError
 
-		"""
-		if callable(function): 
-			result = self.bind(function)
-			if not isinstance(result, Monad): raise TypeError("Operator '>>' must return a Monad instance.")
-			return result
-		else:
-			if not isinstance(function, Monad): raise TypeError("Operator '>>' must return a Monad instance.")
-			return self.bind(lambda _: function)
+    def then(self, function):
+        """ Combines the functionality of bind and fmap.
+
+        Instead of worrying about whether to use bind or fmap,
+        users can just use the then method to chain function
+        calls together. The then method uses attempts to use
+        bind first and if that doesn't work, uses fmap
+        instead.
+
+        Args:
+        function: A python function or lambda expression
+        which returns either a build-in type (int, string,
+        etc.) or an appropriate monad type (Maybe, Either,
+        etc.)
+
+        Returns:
+        A monad value of the same type as 'self'
+        """
+
+        result = self.bind(function)
+        try:
+            result.is_monad_value
+            return result
+        except AttributeError:
+            return self.fmap(function)
+
+    def __rshift__(self, function):
+        """ 
+        The 'bind' operator. The following are equivalent:
+            monadValue >> someFunction
+            monadValue.bind(someFunction)
+
+        """
+        if callable(function): 
+            result = self.bind(function)
+            if not isinstance(result, Monad): raise TypeError("Operator '>>' must return a Monad instance.")
+            return result
+        else:
+            if not isinstance(function, Monad): raise TypeError("Operator '>>' must return a Monad instance.")
+            return self.bind(lambda _: function)
+
+def unit(aClass, value):
+    """ Calls the 'unit' method of 'aClass' with 'value'.  """
+    return aClass.unit(value)
--- pymonad/Monoid.py
+++ pymonad/Monoid.py
@@ -2,86 +2,89 @@
 # (c) Copyright 2014 by Jason DeLaat. 
 # Licensed under BSD 3-clause licence.
 # --------------------------------------------------------
-from pymonad.Container import *
 from pymonad.Reader import curry
 
-class Monoid(Container):
-	"""
-	Represents a data type which conforms to the following conditions:
-	   1. Has an operation (called 'mplus') which combines two values of this type.
-	   2. Has a value (called 'mzero') such that mplus(mzero, value) == mplus(value, mzero) = value.
-	      In other words, mzero acts as an identity element under the mplus operation.
-	   3. mplus is associative: mplus(a, mplus(b, c)) == mplus(mplus(a, b), c)
+class Monoid:
+    """
+    Represents a data type which conforms to the following conditions:
+       1. Has an operation (called 'mplus') which combines two values of this type.
+       2. Has a value (called 'mzero') such that mplus(mzero, value) == mplus(value, mzero) = value.
+          In other words, mzero acts as an identity element under the mplus operation.
+       3. mplus is associative: mplus(a, mplus(b, c)) == mplus(mplus(a, b), c)
 
-	   For instance, integers can be monoids in two ways: With mzero = 0 and mplus = + (addition)
-	   or with mzero = 1 and mplus = * (multiplication).
-	   In the case of strings, mzero = "" (the empty string) and mplus = + (concatenation).
+       For instance, integers can be monoids in two ways: With mzero = 0 and mplus = + (addition)
+       or with mzero = 1 and mplus = * (multiplication).
+       In the case of strings, mzero = "" (the empty string) and mplus = + (concatenation).
 
-	"""
+    """
 
-	def __init__(self, value):
-		""" Initializes the monoid element to 'value'.  """
-		super(Monoid, self).__init__(value)
-	
-	def __add__(self, other):
-		""" The 'mplus' operator.  """
-		return self.mplus(other)
+    def __init__(self, value):
+        """ Initializes the monoid element to 'value'.  """
+        self.value = value
+    
+    def __add__(self, other):
+        """ The 'mplus' operator.  """
+        return self.mplus(other)
 
-	@staticmethod
-	def mzero():
-		"""
-		A static method which simply returns the identity value for the monoid type.
-		This method must be overridden in subclasses to create custom monoids.
-		See also: the mzero function.
+    def __eq__(self, other):
+        return self.value == other.value
 
-		"""
-		raise NotImplementedError
 
-	def mplus(self, other):
-		"""
-		The defining operation of the monoid. This method must be overridden in subclasses
-		and should meet the following conditions.
-		   1. x + 0 == 0 + x == x
-		   2. (x + y) + z == x + (y + z) == x + y + z
-		Where 'x', 'y', and 'z' are monoid values, '0' is the mzero (the identity value) and '+' 
-		is mplus.
+    @staticmethod
+    def mzero():
+        """
+        A static method which simply returns the identity value for the monoid type.
+        This method must be overridden in subclasses to create custom monoids.
+        See also: the mzero function.
 
-		"""
-		raise NotImplementedError
+        """
+        raise NotImplementedError
+
+    def mplus(self, other):
+        """
+        The defining operation of the monoid. This method must be overridden in subclasses
+        and should meet the following conditions.
+           1. x + 0 == 0 + x == x
+           2. (x + y) + z == x + (y + z) == x + y + z
+        Where 'x', 'y', and 'z' are monoid values, '0' is the mzero (the identity value) and '+' 
+        is mplus.
+
+        """
+        raise NotImplementedError
 
 @curry
 def mzero(monoid_type):
-	"""
-	Returns the identity value for monoid_type. Raises TypeError if monoid_type is not a valid monoid.
+    """
+    Returns the identity value for monoid_type. Raises TypeError if monoid_type is not a valid monoid.
 
-	There are a number of builtin types that can operate as monoids and they can be used as such
-	as is. These "natural" monoids are: int, float, str, and list.
-	While thee mzero method will work on monoids derived from the Monoid class,
-	this mzero function will work for *all* monoid types, including the "natural" monoids.
-	For this reason it is preferable to call this function rather than calling the mzero method directly
-	unless you know for sure what type of monoid you're dealing with.
+    There are a number of builtin types that can operate as monoids and they can be used as such
+    as is. These "natural" monoids are: int, float, str, and list.
+    While thee mzero method will work on monoids derived from the Monoid class,
+    this mzero function will work for *all* monoid types, including the "natural" monoids.
+    For this reason it is preferable to call this function rather than calling the mzero method directly
+    unless you know for sure what type of monoid you're dealing with.
 
-	"""
-	try:
-		return monoid_type.mzero()
-	except AttributeError:
-		if isinstance(monoid_type, int) or isinstance(monoid_type, float) or monoid_type == int or monoid_type == float:
-			return 0
-		elif isinstance(monoid_type, str) or monoid_type == str:
-			return ""
-		elif isinstance(monoid_type, list) or monoid_type == list:
-			return []
-		else:
-			raise TypeError(str(monoid_type) + " is not a Monoid.")
+    """
+    try:
+        return monoid_type.mzero()
+    except AttributeError:
+        if isinstance(monoid_type, int) or isinstance(monoid_type, float) or monoid_type == int or monoid_type == float:
+            return 0
+        elif isinstance(monoid_type, str) or monoid_type == str:
+            return ""
+        elif isinstance(monoid_type, list) or monoid_type == list:
+            return []
+        else:
+            raise TypeError(str(monoid_type) + " is not a Monoid.")
 
 @curry
 def mconcat(monoid_list):
-	"""
-	Takes a list of monoid values and reduces them to a single value by applying the
-	mplus operation to each all elements of the list.
+    """
+    Takes a list of monoid values and reduces them to a single value by applying the
+    mplus operation to each all elements of the list.
 
-	"""
-	result = mzero(monoid_list[0])
-	for value in monoid_list:
-		result += value
-	return result
+    """
+    result = mzero(monoid_list[0])
+    for value in monoid_list:
+        result += value
+    return result
