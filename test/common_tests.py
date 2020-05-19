6cb57b58570ea279b7f3ba62016b980a4bf0271e
Jason DeLaat <jason.delaat@gmail.com>
2020-05-19 12:59:27 -0400

    Implemented Monad laws tests in common_tests module.

--- test/common_tests.py
+++ test/common_tests.py
@@ -6,6 +6,7 @@ import pymonad.tools
 
 id = pymonad.tools.identity
 curry = pymonad.tools.curry
+k_compose = pymonad.tools.kleisli_compose
 
 @curry(2)
 def add(x, y):
@@ -19,9 +20,48 @@ def mul(x, y):
 def sub(x, y):
     return x - y
 
+@curry(2)
+def k_inc(cls, x):
+    return cls.insert(x + 1)
+
+@curry(2)
+def k_dec(cls, x):
+    return cls.insert(x - 1)
+
+@curry(2)
+def k_dbl(cls, x):
+    return cls.insert(2*x)
+
+class MonadTests:
+    def setUp(self):
+        raise NotImplementedError('MonadTests: You need to set self._class to the monad class being tested.')
+
+    def test_left_identity(self):
+        inc = k_inc(self._class)
+        self.assertEqual(
+            k_compose(self._class.insert, inc)(0),
+            inc(0)
+        )
+
+    def test_right_identity(self):
+        inc = k_inc(self._class)
+        self.assertEqual(
+            k_compose(inc, self._class.insert)(0),
+            inc(0)
+        )
+
+    def test_associativity(self):
+        inc = k_inc(self._class)
+        dec = k_dec(self._class)
+        dbl = k_dbl(self._class)
+        self.assertEqual(
+            k_compose(k_compose(inc, dec), dbl)(0),
+            k_compose(inc, k_compose(dec, dbl))(0)
+        )
+
 class FunctorTests:
     def setUp(self):
-        raise NotImplementedError('You need to set self._class to the monad class being tested.')
+        raise NotImplementedError('FunctorTests: You need to set self._class to the monad class being tested.')
 
     def test_left_identity(self):
         self.assertEqual(self._class.insert(id(1)), self._class.insert(1))
