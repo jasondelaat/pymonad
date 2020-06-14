09e17703117d1daea82d39ed152833ded5cb25ee
Jason DeLaat <jason.delaat@gmail.com>
2020-06-14 06:24:29 -0400

    Added type hints to operators.maybe module.

--- pymonad/operators/maybe.py
+++ pymonad/operators/maybe.py
@@ -3,14 +3,18 @@
 # Licensed under BSD 3-clause licence.
 # --------------------------------------------------------
 """ Adds operators to the Maybe monad. """
+from typing import TypeVar
+
 import pymonad.maybe
 import pymonad.monad
 import pymonad.operators.operators
 
-class Maybe(pymonad.operators.operators.MonadOperators, pymonad.maybe.Maybe):
+T = TypeVar('T') # pylint: disable=invalid-name
+
+class Maybe(pymonad.operators.operators.MonadOperators, pymonad.maybe.Maybe[T]):
     """ See pymonad.operators.operators and pymonad.maybe. """
 
-def Just(value): # pylint: disable=invalid-name
+def Just(value: T) -> Maybe[T]: # pylint: disable=invalid-name
     """ A Maybe object representing the presence of an optional value. """
     return Maybe(value, True)
 
@@ -21,11 +25,11 @@ Nothing = Maybe(None, False) # pylint: disable=invalid-name
 
 
 
-class Option(Maybe): # pylint: disable=too-many-ancestors
+class Option(Maybe[T]): # pylint: disable=too-many-ancestors
     """ An alias for the Maybe monad class. """
     def __repr__(self):
-        return f'Some {self.value}' if self.monoid else 'Nothing'
+        return f'Some {self.value}' if self.monoid else 'Nothing' # pylint: disable=no-member
 
-def Some(value): # pylint: disable=invalid-name
+def Some(value: T) -> Option[T]: # pylint: disable=invalid-name
     """ An Option object representing the presence of an optional value. """
     return Option(value, True)
