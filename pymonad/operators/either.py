300d56089ef68c6bae3768749b5877fd9e462731
Jason DeLaat <jason.delaat@gmail.com>
2020-06-14 06:28:24 -0400

    Added type hints to operators.either module.

--- pymonad/operators/either.py
+++ pymonad/operators/either.py
@@ -3,18 +3,23 @@
 # Licensed under BSD 3-clause licence.
 # --------------------------------------------------------
 """ Adds operators to the Either monad. """
+from typing import Any, TypeVar
+
 import pymonad.either
 import pymonad.monad
 import pymonad.operators.operators
 
-class Either(pymonad.operators.operators.MonadOperators, pymonad.either.Either):
+S = TypeVar('S') # pylint: disable=invalid-name
+T = TypeVar('T') # pylint: disable=invalid-name
+
+class Either(pymonad.operators.operators.MonadOperators, pymonad.either.Either[S, T]):
     """ See pymonad.operators.operators and pymonad.either. """
 
-def Left(value): # pylint: disable=invalid-name
+def Left(value: S) -> Either[S, Any]: # pylint: disable=invalid-name
     """ Creates a value of the first possible type in the Either monad. """
     return Either(None, (value, False))
 
-def Right(value): # pylint: disable=invalid-name
+def Right(value: T) -> Either[Any, T]: # pylint: disable=invalid-name
     """ Creates a value of the second possible type in the Either monad. """
     return Either(value, (None, True))
 
@@ -24,15 +29,15 @@ def Right(value): # pylint: disable=invalid-name
 
 
 
-class _Error(Either): # pylint: disable=too-many-ancestors
+class _Error(Either[S, T]): # pylint: disable=too-many-ancestors
     def __repr__(self):
-        return f'Result: {self.value}' if self.is_right() else f'Error: {self.monoid[0]}'
+        return f'Result: {self.value}' if self.is_right() else f'Error: {self.monoid[0]}' # pylint: disable=no-member
 
-def Error(value): # pylint: disable=invalid-name
+def Error(value: S) -> _Error[S, Any]: # pylint: disable=invalid-name
     """ Creates an error value as the result of a calculation. """
     return _Error(None, (value, False))
 
-def Result(value): # pylint: disable=invalid-name
+def Result(value: T) -> _Error[Any, T]: # pylint: disable=invalid-name
     """ Creates a value representing the successful result of a calculation. """
     return _Error(value, (None, True))
 
