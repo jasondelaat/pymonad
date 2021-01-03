9e161f158ddff0325f24929881dfc79e3689ada1
Jason DeLaat <jason.delaat@gmail.com>
2021-01-03 18:53:54 -0500

    Changed version to 2.3.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as readme:
 
 setuptools.setup(
     name='PyMonad',
-    version='2.3.0',
+    version='2.3.1',
     author='Jason DeLaat',
     author_email='jason.develops@gmail.com',
     packages=setuptools.find_packages(),
